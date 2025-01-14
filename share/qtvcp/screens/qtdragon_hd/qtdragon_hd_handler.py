c996d9302eafaa27e5a03c6223ae64ee201f5a87
Andy Lewis <gardenweazel@yahoo.com>
2025-01-14 16:13:24 -0600

    ELIF typo
    Changed line 676 from IF to ELIF

--- share/qtvcp/screens/qtdragon_hd/qtdragon_hd_handler.py
+++ share/qtvcp/screens/qtdragon_hd/qtdragon_hd_handler.py
@@ -667,7 +667,7 @@ class HandlerClass:
                             receiver.keyPressEvent(event)
                             event.accept()
                         return True
-                if is_pressed:
+                elif is_pressed:
                     receiver.keyPressEvent(event)
                     event.accept()
                     return True
